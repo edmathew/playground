47ee1ac7c765425efeac85586efe5cf02f7f05ac
Edgar Mateus <ejpmateus@gmail.com>
2017-03-06 22:29:18 +0000

    Refactor problem 4 with TDD

--- euler/java/src/main/java/com/ejpm/euler/problem/impl/Problem4.java
+++ euler/java/src/main/java/com/ejpm/euler/problem/impl/Problem4.java
@@ -1,43 +1,40 @@
 package com.ejpm.euler.problem.impl;
 
+import com.ejpm.euler.math.PalindromeUtils;
 import com.ejpm.euler.problem.Problem;
- 
-	/**
-	 * A palindromic number reads the same both ways.
-	 *  The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-	 *  Find the largest palindrome made from the product of two 3-digit numbers.
-	 */
+
+/**
+ * A palindromic number reads the same both ways. The largest palindrome made
+ * from the product of two 2-digit numbers is 9009 = 91 × 99. Find the largest
+ * palindrome made from the product of two 3-digit numbers.
+ */
 public class Problem4 implements Problem {
 
     @Override
     public void execute() {
-		int Lproduct = 0;
+        System.out.println("Problem 4:\t The largest\n"
+                + " * palindrome made from the product of two 3-digit number " + calcLargestProductPalindrome(3));
 
-		for(int i = 100; i < 1000;i++){
-			for(int j = 100; j < 1000; j++){
-				int result = i * j;
-				String aux = ""+result;
-				if(isPalindrome(aux)&& result > Lproduct){
-					Lproduct = result;
-				}
-			}
-		}
-
-		System.out.println("Largest Palindrome: "+Lproduct);
     }
 
-	public static boolean isPalindrome(String a){
-		if(a.length() != 6)
-			return false;
-		else{
-			String aux1 = a.substring(0, 3);
-			String aux2 = a.substring(3);
-			String inv = "";
-			for(int i = aux2.length()-1; i>=0; i -- ){
-				inv += aux2.charAt(i);
-			}
-			return aux1.equals(inv);
-		}
-	}
+    public int calcLargestProductPalindrome(int nDigits) {
+        final int bottom = (int) (Math.pow(10, nDigits - 1));
+        final int top = (int) (Math.pow(10, nDigits));
+
+        int result = 0;
+
+        for (int i = top - 1; i >= bottom; i--) {
+            for (int j = top - 1; j >= bottom; j--) {
+                final int product = i * j;
+                if (product > result) {
+                    if (PalindromeUtils.isPalindrome(product)) {
+                        result = product;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
 
 }
