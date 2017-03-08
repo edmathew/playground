885b93a9a91f7ef006ae7cb7571e9d5334c12cff
Edgar Mateus <ejpmateus@gmail.com>
2017-03-08 13:30:33 +0000

    Implement prime number sum sequence

--- euler/java/src/main/java/com/ejpm/euler/math/SequencesSum.java
+++ euler/java/src/main/java/com/ejpm/euler/math/SequencesSum.java
@@ -1,6 +1,8 @@
 
 package com.ejpm.euler.math;
 
+import com.ejpm.euler.math.prime.PrimeSequence;
+
 /**
  *
  * @author edgar.mateus
@@ -17,6 +19,19 @@ public class SequencesSum {
         final int qtd = (top - 1)/divisible;
         
         return divisible * (qtd * (qtd + 1)) / 2;
+    }
+    
+    public static long sumAllPrimeNumbersUnder(final int top){
+        final PrimeSequence sequence = new PrimeSequence();
+        long sum = 0;
+        int prime = sequence.getNext();
+        
+        while(prime < top){
+            sum += prime;
+            prime = sequence.getNext();
+        }
+        
+        return sum;
     }
 
 }
--- euler/java/src/test/java/com/ejpm/euler/math/SequencesSumTest.java
+++ euler/java/src/test/java/com/ejpm/euler/math/SequencesSumTest.java
@@ -15,14 +15,24 @@ public class SequencesSumTest {
     public void theSumOfAllDivisibleOf3Under1000Is() {
         assertThat(SequencesSum.sumAllDivisibleBy(3, 1000), is(equalTo(166833)));
     }
-    
+
     @Test
     public void theSumOfAllDivisibleOf5Under1000Is() {
         assertThat(SequencesSum.sumAllDivisibleBy(5, 1000), is(equalTo(99500)));
     }
-    
+
     @Test
     public void theSumOfAllDivisibleOf15Under1000Is() {
         assertThat(SequencesSum.sumAllDivisibleBy(15, 1000), is(equalTo(33165)));
     }
-} //233168
+
+    @Test
+    public void theSumofAllPrimeNumbersUnder10is17() {
+        assertThat(SequencesSum.sumAllPrimeNumbersUnder(10), is(equalTo(17L)));
+    }
+
+    @Test
+    public void theSumofAllPrimeNumbersUnder2Milions17() {
+        assertThat(SequencesSum.sumAllPrimeNumbersUnder(2000000), is(equalTo(142913828922L)));
+    }
+}
