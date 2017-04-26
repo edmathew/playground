84e6e416fa81b036c4162aa4e591159f67d50025
Edgar Mateus <ejpmateus@gmail.com>
2017-04-26 19:46:03 +0100

    Mongodb movies db insert and increment Java project setup

---
+++ learning/java/mongodb/src/main/java/com/ejpm/learn/App.java
@@ -0,0 +1,17 @@
+package com.ejpm.learn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        System.out.println("Mongo learn app");
+        System.out.println("DONE");
+    }
+ 
+
+ 
+}
