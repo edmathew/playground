c8f8c5eb684cdbc3dd2fcc118b1cbed69c1ac728
Edgar Mateus <ejpmateus@gmail.com>
2017-11-06 22:04:41 +0000

    learn drools - add kie Session Helper

---
+++ learning/java/drools/src/main/java/pt/ejpm/learn/drools/knowledge/KnowledgeSessionHelper.java
@@ -0,0 +1,27 @@
+package pt.ejpm.learn.drools.knowledge;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.StatelessKieSession;
+
+/**
+ *
+ * @author edgar.mateus
+ */
+public class KnowledgeSessionHelper {
+
+    public static KieContainer createRuleBase() {
+        final KieServices ks = KieServices.Factory.get();
+        return ks.getKieClasspathContainer();
+    }
+
+    public static StatelessKieSession getStatelessKnowledgeSession(final KieContainer kieContainer, final String sessionName) {
+        return kieContainer.newStatelessKieSession(sessionName);
+    }
+
+    public static KieSession getStatefullKnowledgeSession(final KieContainer kieContainer, final String sessionName) {
+        return kieContainer.newKieSession();
+    }
+
+}
